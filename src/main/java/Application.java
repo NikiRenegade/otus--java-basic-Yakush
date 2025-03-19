26a94aef740ecf1b75d9c668b8957cb7e8c8c91d
NikiRenegade <nikirenegade@gmail.com>
2025-03-19 22:17:43 +0700

    Реализовано пятое ДЗ

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Cat lisik = new Cat("Lisik", 2, 1 );
+        lisik.run(5);
+        Dog smoke = new Dog("Smoke", 2, 6, 15);
+        smoke.swim(180);
+        smoke.getInfo();
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat extends Animal {
+    public Cat(String name, int runSpeed, int stamina) {
+        super(name, runSpeed, 0, stamina, null);
+    }
+}
