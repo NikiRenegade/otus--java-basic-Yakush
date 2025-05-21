8c18eb0bbf62e93cadf6b016836ca12c1a6c99b7
NikiRenegade <nikirenegade@gmail.com>
2025-05-21 22:46:17 +0700

    Реализовано дз 18

---
+++ otherHomework/src/hw18/Box.java
@@ -0,0 +1,29 @@
+package hw18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    private List<T> fruits = new ArrayList<>();
+
+    private void addFruits(List<T> fruitsToAdd) {
+        fruits.addAll(fruitsToAdd);
+    }
+    public void addFruit(T fruit) {
+        fruits.add(fruit);
+    }
+    public float getWeight() {
+        float weight = 0.0f;
+        for (T fruit : fruits) {
+            weight += fruit.getWeight();
+        }
+        return weight;
+    }
+    public boolean compare(Box<? extends Fruit> otherBox) {
+        return Math.abs(this.getWeight() - otherBox.getWeight()) < 0.0001f;
+    }
+    public void transferFruits(Box<T> otherBox) {
+        otherBox.addFruits(this.fruits);
+        this.fruits.clear();
+    }
+}
---
+++ otherHomework/src/hw18/Orange.java
@@ -0,0 +1,8 @@
+package hw18;
+
+public class Orange extends Fruit {
+    @Override
+    public float getWeight() {
+        return 1.5f; // Вес одного яблока
+    }
+}
